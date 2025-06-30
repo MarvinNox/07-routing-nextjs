e4b365f8f421d52b4d9a7ac008083d1d01dc70d0
Marvin Nox <sinedtex@gmail.com>
2025-06-30 11:29:14 +0300

    update--tagsMenu.tsx

--- components/TagsMenu/TagsMenu.tsx
+++ components/TagsMenu/TagsMenu.tsx
@@ -1,15 +1,35 @@
 "use client";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import css from "./TagsMenu.module.css";
 import Link from "next/link";
 import { tagMenu } from "@/app/const/constants";
 
 export default function TagsMenu() {
   const [isOpen, setIsOpen] = useState(false);
+  const menuRef = useRef<HTMLDivElement>(null);
+
   const toggleMenu = () => setIsOpen(!isOpen);
 
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (menuRef.current && !menuRef.current.contains(event.target as Node)) {
+        setIsOpen(false);
+      }
+    };
+
+    if (isOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    } else {
+      document.removeEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isOpen]);
+
   return (
-    <div className={css.menuContainer}>
+    <div ref={menuRef} className={css.menuContainer}>
       <button onClick={toggleMenu} className={css.menuButton}>
         Notes ▾
       </button>
